12bb78305d36df1f8e5a7e22f842fad1504f15cc
Jursley Gonzalez <Freaqke@outlook.com>
2019-05-31 15:50:57 +0200

    Added Login and Registration Controller and Views

---
+++ Foodsquare/Controllers/LoginController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Foodsquare.Models;
+
+namespace Foodsquare.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
